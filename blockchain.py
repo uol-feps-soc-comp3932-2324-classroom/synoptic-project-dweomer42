e445f8d49e0fe584e210d116e0124f4e12367456
Joseph Harros <sc20jh@leeds.ac.uk>
2024-03-08 09:41:19 +0000

    Add initial method skeletons for Blockchain class

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,29 @@
 from flask import Flask
+
+class Blockchain():
+  def init(self):
+    self.chain = []
+    self.currentTransactions = []
+
+  def createBlock(self):
+    # Creates a block and adds it to the chain
+    pass
+
+  def newTransaction(self):
+    # Adds a new transaction to the list of transactions
+    pass
+
+  @staticmethod
+  def __hash__(self):
+    # Hashes a block
+    pass
+
+  @property
+  def lastBlock(self):
+    # Returns the last block in the chain
+    pass
+
+
 app = Flask(__name__)
 
 @app.route("/")
