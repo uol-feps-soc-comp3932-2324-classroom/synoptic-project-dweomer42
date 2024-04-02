80117d31efea87a244a4a430d1f6781f9b1123c0
Joseph Harros <sc20jh@leeds.ac.uk>
2024-04-02 15:29:26 +0100

    Made registration optional on ports for server code and added a method to mine and validate blocks in parallel for further testing

--- server.py
+++ server.py
@@ -1,17 +1,58 @@
 import json
 from urllib.parse import urlparse
 import requests
+from multiprocessing import process
+import multiprocessing
 
-requestArray = []
-# Register everything
-for i in range(5000,5006):
-    requestString = "http://localhost:" + str(i)
-    requestArray.append(requestString)
+def mineBlock(port):
+    requests.get(f'http://localhost:{port}/mine')
+    response = requests.get(f'http://localhost:{port}/nodes/resolve')
+    #print(response.text)
 
-for i in range(5000,5006):
-    #print(requestArray)
-    payload = json.dumps({'nodes':requestArray})
-    myjson = {'nodes':requestArray}
-    print(json.loads(payload))
-    response = requests.post(f'http://localhost:{i}/nodes/register',json=myjson)
+
+
+
+
+def register(startPort, endPort):
+    requestArray = []
+    # Register everything
+    for i in range(startPort,endPort):
+        requestString = "http://localhost:" + str(i)
+        requestArray.append(requestString)
+
+    for i in range(startPort,endPort):
+        #print(requestArray)
+        payload = json.dumps({'nodes':requestArray})
+        myjson = {'nodes':requestArray}
+        #print(json.loads(payload))
+        response = requests.post(f'http://localhost:{i}/nodes/register',json=myjson)
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser 
+    
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--startPort', default=5000, type=int, help='first port registered')
+    parser.add_argument('-e', '--endPort', default=5005, type=int, help='last port registered')
+    parser.add_argument('-r', '--register', default=False, type=bool, help='set to true to register ports')
+    args = parser.parse_args()
+    startPort = args.startPort
+    endPort = args.endPort
+    shouldReg = args.register
+    if(shouldReg == True):
+        register(startPort,endPort)
+    # input list 
+    inputs = range(startPort,endPort)
+    
+    # multiprocessing pool object 
+    pool = multiprocessing.Pool() 
+  
+    # pool object with number of element 
+    pool = multiprocessing.Pool(processes=endPort-startPort) 
+  
+    # map the function to the list and pass 
+    # function and input list as arguments 
+    outputs = pool.map(mineBlock, inputs)
+
+    # Print output list 
+    print("Output: {}".format(outputs))  
     
